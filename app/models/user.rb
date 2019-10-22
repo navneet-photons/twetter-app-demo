eff611fc91dcb2c77050475591fdbdb13e36cff7
Navneet Shirke <nav.s@photons.ai>
2019-10-22 20:47:23 +0530

    sessions changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-  :recoverable, :rememberable, :validatable
+  :recoverable, :rememberable, :validatable, :token_authenticatable
   has_many :tweets
   has_many :follows
   before_create :generate_auth_token
